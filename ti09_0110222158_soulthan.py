6615d4e7658423a7c12e0658847d2a0e693f309c
soulthanti04 <fadhlillahbagussoulthan@gmail.com>
2022-10-28 13:29:41 +0700

    first commit

---
+++ ti09_0110222158_soulthan.py
@@ -0,0 +1,17 @@
+hasil = 0
+n = 17
+for i in range(13, n+1):
+    hasil = hasil + 5*i + 2
+print(hasil) 
+
+hasil = 0
+n = 10
+for n in range(5, n+1):
+    hasil = hasil + n*n + 2*n - 1
+print(hasil) 
+
+hasil = 1
+n = 8
+for i in range(4, n+1):
+    hasil = hasil * (i - 3)
+print(hasil) 
